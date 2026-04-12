ada06a9a4c16e9592d89de46fb595d0a95f85e2d
emilio lugo <138060242+emiliolugo@users.noreply.github.com>
2026-04-12 11:15:22 -0400

    Add: n-queens - submission-0

---
+++ Data Structures & Algorithms/n-queens/submission-0.py
@@ -0,0 +1,32 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        ret = []
+        def helper(i: int, seen: List[set([])], curr: List[str]) -> None:
+            if i == n:
+                if len(curr) == n:
+                    ret.append(curr[:])
+                return
+            
+            for j in range(n):
+                if j in seen[0]:
+                    continue
+                if i-j in seen[1]:
+                    continue
+                if i+j in seen[2]:
+                    continue
+                seen[0].add(j)
+                seen[1].add(i-j)
+                seen[2].add(i+j)
+                curr.append("." * j + "Q" + "." * (n-1-j))
+                helper(i+1, seen, curr)
+                curr.pop()
+                seen[0].remove(j)
+                seen[1].remove(i-j)
+                seen[2].remove(i+j)
+            return
+
+        helper(0, [set([]),set([]),set([])], [])
+        return ret
+
+
+    
