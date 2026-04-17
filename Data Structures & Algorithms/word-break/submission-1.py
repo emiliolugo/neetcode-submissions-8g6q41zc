24ad369ed28ffa22d65630a27cd3d4ffa2ac6b98
emilio lugo <138060242+emiliolugo@users.noreply.github.com>
2026-04-16 21:24:50 -0400

    Add: word-break - submission-1

---
+++ Data Structures & Algorithms/word-break/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        m = [[] for _ in range(26)]
+        for word in wordDict:
+            m[ord(word[0])-ord('a')].append(word)
+
+        canreach = [False] * (len(s)+1)
+        canreach[0] = True
+        for i in range(len(s)):
+            if not canreach[i]:
+                continue
+            for word in m[ord(s[i])-ord('a')]:
+                if len(word) + i  - 1< len(s) and s[i:i+len(word)] == word:
+                    canreach[i+len(word)] = True
+                    if canreach[-1]:
+                        return True
+        
+        return False
+
+        
+        
