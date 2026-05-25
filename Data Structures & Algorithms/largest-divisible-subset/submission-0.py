d008bb67b19179eb4975e3890343b09eb746bde0
emilio lugo <138060242+emiliolugo@users.noreply.github.com>
2026-05-25 11:06:17 -0400

    Add: largest-divisible-subset - submission-0

---
+++ Data Structures & Algorithms/largest-divisible-subset/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        nums.sort()
+
+        dp = [[] for _ in range(len(nums))]
+        for i in range(len(nums)):
+            for j in range(i):
+                if (nums[j] == 0 or nums[i] % nums[j] == 0) and len(dp[j]) > len(dp[i]):
+                    dp[i] = dp[j][:]
+
+            dp[i].append(nums[i])
+        
+        ret = []
+        for ls in dp:
+            if len(ls) > len(ret):
+                ret = ls[:]
+        return ret
+                
+                
+        
